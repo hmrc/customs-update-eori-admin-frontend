4e8e9c017cf048e6331b5fdc9af6f06573164243
tolgatunaaa <tolga.tuna@digital.hmrc.gov.uk>
2022-12-05 17:21:57 +0000

    - Config changes done - UI and Controller changes done

--- app/uk/gov/hmrc/customsupdateeoriadminfrontend/controllers/EoriActionController.scala
+++ app/uk/gov/hmrc/customsupdateeoriadminfrontend/controllers/EoriActionController.scala
@@ -5,38 +5,33 @@
 
 package uk.gov.hmrc.customsupdateeoriadminfrontend.controllers
 
-import com.sun.xml.internal.bind.v2.TODO
 import play.api.data.Form
-import play.api.data.Forms._
+import play.api.data.Forms.{mapping, text}
 import play.api.i18n.I18nSupport
-import play.api.mvc.ControllerHelpers.TODO
-import play.api.mvc.MessagesControllerComponents
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.customsupdateeoriadminfrontend.model.EoriAction
 import uk.gov.hmrc.customsupdateeoriadminfrontend.views.html.eori_action
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
-import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.Future
 
-case class EoriActionController @Inject()(
-     mcc: MessagesControllerComponents,
-     viewEoriAction: eori_action)(implicit ec: ExecutionContext)
-  extends FrontendController(mcc)
-    with I18nSupport {
+@Singleton
+case class EoriActionController @Inject()(mcc: MessagesControllerComponents,
+                                          viewEoriAction: eori_action)
+  extends FrontendController(mcc) with I18nSupport {
 
-  val form = Form(
-    mapping(
-      "update-or-cancel-eori" -> text
-    )(EoriAction.apply)(EoriAction.unapply))
+  val form: Form[EoriAction] = Form(mapping(
+    "update-or-cancel-eori" -> text()
+  )(EoriAction.apply)(EoriAction.unapply))
 
-  def showPage = Action { implicit request =>
-   Ok(viewEoriAction(form))
-    //Ok("In show page of eori action")
+  def showPage(): Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(viewEoriAction(form)))
   }
 
-  def continueAction = Action { implicit request =>
-    Ok("In continue")
-   }
+  def continueAction(): Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok("continue"))
+  }
 }
 
 
--- app/uk/gov/hmrc/customsupdateeoriadminfrontend/model/EoriUpdate.scala
+++ app/uk/gov/hmrc/customsupdateeoriadminfrontend/model/EoriUpdate.scala
@@ -5,6 +5,4 @@
 
 package uk.gov.hmrc.customsupdateeoriadminfrontend.model
 
-import java.time.LocalDate
-
 case class EoriUpdate(existingEori: String, date: String, newEori: String)
