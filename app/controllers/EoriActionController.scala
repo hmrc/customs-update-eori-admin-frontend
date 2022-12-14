5c57a9d77e17e4a7a0899b93b6247dd49aff2c8d
ajayshirke <ajay.shirke@digital.hmrc.gov.uk>
2022-12-14 14:47:44 +0000

    Sprint-1 : ETD-25 Changes
    Changes for below story.
    ETD-25: Front-end - Update EORI number or remove enrolment question

---
+++ app/controllers/EoriActionController.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ */
+
+package controllers
+
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import models.EoriAction
+import views.html.EoriActionView
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.Future
+
+@Singleton
+case class EoriActionController @Inject()(mcc: MessagesControllerComponents,
+                                          viewEoriAction: EoriActionView)
+  extends FrontendController(mcc) with I18nSupport {
+
+  val form: Form[EoriAction] = Form(mapping(
+    "update-or-cancel-eori" -> text()
+  )(EoriAction.apply)(EoriAction.unapply))
+
+  def showPage() = Action.async { implicit request =>
+    Future.successful(Ok(viewEoriAction(form)))
+  }
+
+  /*
+  def continueAction() = Action { implicit request =>
+    val formContent = request.body.asFormUrlEncoded
+    Future.successful(Ok(viewEoriAction(form)))
+
+    formContent.map { args =>
+      val actionSelected = args("update-or-cancel-eori").head
+       if (actionSelected != null && actionSelected == "updateeori")  {
+        Redirect(controllers.routes.UpdateEoriController.showPage())
+      }
+      else if (actionSelected != null && actionSelected == "canceleori"){
+        Redirect(uk.gov.hmrc.customsupdateeoriadminfrontend.controllers.routes.CancelEnrolmentsController.showPage())
+      }
+      else Redirect(uk.gov.hmrc.customsupdateeoriadminfrontend.controllers.routes.EoriActionController.showPage())
+    }.getOrElse(
+      Redirect(uk.gov.hmrc.customsupdateeoriadminfrontend.controllers.routes.EoriActionController.showPage()))
+
+  } */
+}
+
+
+
---
+++ app/models/EoriAction.scala
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ */
+
+package models
+
+case class EoriAction(cancelOrUpdate: String)
