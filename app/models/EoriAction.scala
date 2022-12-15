02c9dd7f351f9755d84d3efce29429d09ce74203
ajayshirke22 <ajay.shirke@digital.hmrc.gov.uk>
2022-12-15 15:56:45 +0000

    ETD-25 changes
    ETD-25 changes

--- app/models/EoriAction.scala
+++ app/models/EoriAction.scala
@@ -14,20 +14,6 @@
  * limitations under the License.
  */
 
-package config
+package models
 
-import javax.inject.{Inject, Singleton}
-
-import play.api.i18n.MessagesApi
-import play.api.mvc.Request
-import play.twirl.api.Html
-import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
-import views.html.ErrorTemplate
-
-@Singleton
-class ErrorHandler @Inject()(errorTemplate: ErrorTemplate, val messagesApi: MessagesApi)
-    extends FrontendErrorHandler {
-
-  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: Request[_]): Html =
-    errorTemplate(pageTitle, heading, message)
-}
+case class EoriAction(cancelOrUpdate: String)
