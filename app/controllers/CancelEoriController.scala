209c53583305bd43f81331777be59625b0d06547
Hari-M-K <117358739+Hari-M-K@users.noreply.github.com>
2023-01-24 15:39:52 +0000

    etd-36 : new frontend pages for cancel eori journey

---
+++ app/controllers/CancelEoriController.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import models.EoriCancel
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+import service.EnrolmentService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.CancelEoriView
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@Singleton
+case class CancelEoriController @Inject()(mcc: MessagesControllerComponents,
+                                          cancelEoriView: CancelEoriView,
+                                          auth: AuthAction,
+                                          enrolmentService: EnrolmentService
+                                         )(implicit ec: ExecutionContext)
+  extends FrontendController(mcc)
+    with I18nSupport {
+
+  val formCancelEori = Form(
+    mapping(
+      "existing-eori" -> text(),
+      "date-of-establishment-day" -> text(),
+      "date-of-establishment-month" -> text(),
+      "date-of-establishment-year" -> text()
+    )(EoriCancel.apply)(EoriCancel.unapply))
+
+  def showPage = auth { implicit request =>
+    Ok(cancelEoriView(formCancelEori))
+  }
+
+
+}
