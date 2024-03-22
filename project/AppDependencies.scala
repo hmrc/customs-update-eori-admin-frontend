59c0f588d01aa8825076e5232c7d2c535eaa2947
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2024-03-22 11:12:46 +0000

    DDCYLS-4835: Update bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.4.0"
+  private val bootstrapVersion = "8.5.0"
   private val playVersion = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
